05f990e16e941b0eade16fc566271696fef28f8a
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2021-08-21 08:31:26 +0200

    Adding components.
    Adding components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import ChatFeed from './components/ChatFeed';
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="1b10bd3f-22ca-463c-aa73-c3c3ec598a41"
+            userName="FirstUser"
+            userSecret="firstuser"
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
+        />
+    )
 }
 
 export default App;
